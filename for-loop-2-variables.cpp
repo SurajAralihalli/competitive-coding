11e6678ede86d3b5537606102665783db1b00718
suraj aralihalli <suraj.ara16@gmail.com>
2021-05-10 19:52:06 +0530

    longest-palindromic-substring

---
+++ for-loop-2-variables.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i;
+    int j;
+    for (i=0,j=0;i<10 && j<20;i++,j++)
+    {
+        cout << i << " " << j << endl;
+    }
+    return 0;
+}
---
+++ leetcode/longest-palindromic-substring.cpp
@@ -0,0 +1,66 @@
+// Author: Suraj Aralihalli
+// Url: https://leetcode.com/problems/longest-palindromic-substring/
+// Date: 10th May, 2021
+
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        vector< vector<int>> matrix;
+        matrix.assign(s.size(),vector<int>(s.size(),0));
+        int l = s.size();
+        string str = s;
+        int max_s =0;
+        int max_e =0;
+        int max_l = 0;
+        for(int i=0;i<l;i++)
+        {
+            matrix[i][i]=1;
+        }
+        for(int ls=1;ls<=l;ls++)
+        {
+            for(int s=0;s <= l-ls ; s++ )
+            {
+                int e = s + ls -1;
+                if(ls==1)
+                {
+                    matrix[s][e]=1;
+                    max_s = s;
+                    max_e = e;
+                    max_l=ls;
+                    
+                }
+                else if (ls==2)
+                {
+                    if(str[s]==str[e])
+                    {
+                        matrix[s][e]=1;
+                        max_s = s;
+                        max_e = e;
+                        max_l=ls;
+                    }
+                }
+                else
+                {
+                    if((str[s] == str[e]) && (matrix[s+1][e-1]==1))
+                    {
+                        matrix[s][e]=1;
+                        max_s = s;
+                        max_e = e;
+                        max_l=ls;
+                    }
+                }
+            }
+        }
+        // for(int i =0;i<l;i++)
+        // {
+        //     for(int j=0;j<l;j++)
+        //     {
+        //         cout << matrix[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        return s.substr(max_s,max_l);
+        
+    }
+};
