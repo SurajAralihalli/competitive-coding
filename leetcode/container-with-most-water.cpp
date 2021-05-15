b40b0cd38e4d5e8770212452c8ede19def3075f5
suraj aralihalli <suraj.ara16@gmail.com>
2021-05-15 13:26:01 +0530

    container-with-most-water

---
+++ leetcode/container-with-most-water.cpp
@@ -0,0 +1,29 @@
+// Author: Suraj Aralihalli
+// Url: https://leetcode.com/problems/container-with-most-water/
+// Date: 15th May, 2021
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int l =0;
+        int r = n-1;
+        int maxi = 0;
+        while(l<r)
+        {
+            int v = min(height[l],height[r]) * (r-l);
+            
+            if(height[l]>height[r])
+            {
+                r=r-1;
+            }
+            else
+            {
+                l = l +1;
+            }
+            maxi = max(v,maxi);
+            
+        }
+        return maxi;
+    }
+};
