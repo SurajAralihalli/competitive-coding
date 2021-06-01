b7850c767fee830a0dc208765af281c87ecd6a0a
suraj aralihalli <suraj.ara16@gmail.com>
2021-06-01 20:52:06 +0530

    queue

---
+++ notes/queue.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    queue<int>q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+
+    cout << q.front() << endl;
+    cout << q.back() << endl;
+
+
+    q.pop();
+    cout << q.front() << endl;
+
+}
