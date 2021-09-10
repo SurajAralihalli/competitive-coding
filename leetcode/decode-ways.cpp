9369982502dd3b2fe132ca8cb4fc496f605b21cb
Suraj Aralihalli <suraj.ara16@gmail.com>
2021-09-10 15:34:25 -0400

    Update decode-ways.cpp

--- leetcode/decode-ways.cpp
+++ leetcode/decode-ways.cpp
@@ -58,3 +58,60 @@ public:
         return dp[i];
     }
 };
+
+
+
+
+##################################################################################################################
+## Alternative Itrative Logic
+
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n,0);
+        if(n>0 && s[0]!='0')
+        {
+            dp[0]=1;
+        }
+        for(int i=1;i<n;i++)
+        {
+
+                if(s[i-1]>'2' && s[i]!='0')
+                {
+                    dp[i] = dp[i-1];
+                }
+                else
+                {
+                    
+                    if(s[i]=='0')
+                    {
+                        if(s[i-1]>'2' || s[i-1]=='0') return 0;
+                        if(i>=2) dp[i] = dp[i-2];
+                        else dp[i] = dp[i-1];
+                    }
+                    else
+                    {
+                        dp[i] = dp[i-1];
+                        if(((s[i-1]=='2' && s[i]<'7') || s[i-1]=='1'))
+                        {
+                            if(i>=2) dp[i]+= dp[i-2];
+                            else dp[i]+=1;
+                        }
+                    }
+
+                }
+            
+            // for(auto i:dp)
+            // {
+            //     cout << i << " ";
+            // }
+            // cout << endl;
+        }
+        
+        
+        
+        return dp[n-1];
+    }
+};
