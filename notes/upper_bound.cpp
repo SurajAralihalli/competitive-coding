bea16742ba914673f3e21dcf7b5ff195718b48a4
Suraj Aralihalli <suraj.ara16@gmail.com>
2021-10-03 19:27:37 -0400

    Update upper_bound.cpp

--- notes/upper_bound.cpp
+++ notes/upper_bound.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+    /// VECTOR
     vector<int> vec = {1,2,3,4,5,30,70,300,22222};
     
     int add=56;
@@ -20,6 +21,26 @@ int main()
     
     // cout << *l << " " << *r << endl;
     
+    
+    
+    /// SET 
+    set<int> s = {9,4,5,1,-1,7,100};
+    
+    int add=6;
+    s.insert(6);
+    for(auto i:s)
+    {
+        cout << i << endl;
+    }
+    // auto l = lower_bound(s.begin(),s.end(),70); returns iterator to 70
+    
+    auto r = upper_bound(s.begin(),s.end(),70);
+    
+    
+    cout << "upper_bound: " << *r << endl;
+    //returns 100
+
+    return 0;
 
     return 0;
 }
