0d693ffe1eb2d932bc28b529f4a4b02f02de6467
Suraj Aralihalli <suraj.ara16@gmail.com>
2022-07-04 12:11:46 -0400

    Update jump-game.cpp

--- leetcode/jump-game.cpp
+++ leetcode/jump-game.cpp
@@ -2,6 +2,7 @@
 // Url: https://leetcode.com/problems/jump-game/
 // Date: 20th May, 2021
 
+// Approach 1
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
@@ -27,5 +28,44 @@ public:
             return true;
         }
         return false;
+    }
+};
+
+// Approach 2
+class Solution {
+public:
+    vector<int> marked;
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        marked.assign(n,-1);
+        return dp(nums,0)==1?true:false;        
+    }
+    
+    int dp(vector<int>& nums, int index)
+    {
+        if(index>=nums.size()-1) 
+        {
+            return 1;
+        }
+        else if(marked[index]!=-1) 
+        {
+            return marked[index];
+        }
+        else
+        {
+            int flag =0;
+            for(int i=1;i<=nums[index];i++)
+            {
+                if(dp(nums, index+i)==1)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            
+            marked[index] = flag;
+            return marked[index];
+        }
+        
     }
 };
