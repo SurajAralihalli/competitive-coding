1c8df15c378f0e848369a12acc85fbfb0c8a4eca
Suraj Aralihalli <suraj.ara16@gmail.com>
2022-07-12 21:55:23 -0400

    Update lowest-common-ancestor-of-a-binary-tree.cpp

--- leetcode/lowest-common-ancestor-of-a-binary-tree.cpp
+++ leetcode/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -13,6 +13,42 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+//Approach 1
+class Solution {
+public:
+    TreeNode* lca;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        lca = NULL;
+        recursive(root, p, q);
+        return lca;
+    }
+    
+    pair<bool,bool> recursive(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if(root!=NULL)
+        {
+            bool pFound = root->val == p->val?true:false;
+            bool qfound = root->val == q->val?true:false;
+            
+            auto l = recursive(root->left, p, q);
+            auto r = recursive(root->right, p, q);
+            
+            pFound = pFound || l.first || r.first;
+            qfound = qfound || l.second || r.second;
+            
+            
+            if(!lca && pFound && qfound)
+            {
+                lca = root;
+            }
+            return {pFound, qfound};
+        }
+        return {false, false};
+    }
+};
+
+//Approach 2
 class Solution {
 public:
     TreeNode* lca;
