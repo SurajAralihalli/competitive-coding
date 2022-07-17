ab52dd32a2f516ad513cb708709ad3620ce568ae
Suraj Aralihalli <suraj.ara16@gmail.com>
2022-07-17 11:45:17 -0400

    Update coin-change.cpp

--- leetcode/coin-change.cpp
+++ leetcode/coin-change.cpp
@@ -3,7 +3,41 @@
 // Date: 20th June, 2021
 // Tags: dp
 
+//Approach 1
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(amount +1, 0));
+        
+        for(int j=0;j<=amount;j++) 
+        {
+            dp[0][j] = INT_MAX;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                if(j>=coins[i-1])
+                {
+                    
+                    int plusOne = dp[i][j-coins[i-1]] == INT_MAX? INT_MAX : dp[i][j-coins[i-1]] +1;
+                    
+                    dp[i][j] = min(dp[i-1][j], plusOne);
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][amount] == INT_MAX? -1: dp[n][amount];
+    }
+};
 
+//Approach 2
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
