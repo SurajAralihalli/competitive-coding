823833ffa0cde4c0ef6295517879052f7d0a23b5
Suraj Aralihalli <suraj.ara16@gmail.com>
2022-07-17 15:38:27 -0400

    Update non-overlapping-intervals.cpp

--- leetcode/non-overlapping-intervals.cpp
+++ leetcode/non-overlapping-intervals.cpp
@@ -1,47 +1,36 @@
 
 // Author: Suraj Aralihalli
 // Url: https://leetcode.com/problems/non-overlapping-intervals/
-// Date: 6th December, 2021
+// Date: 17th July, 2022
 // Tags: arrays, intervals
 
+// find max number of non overlapping intervals - sort based on end times
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),comp);
-        vector<vector<int>> bag;
-        int noncomp = 0;
-        for(auto i: intervals)
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), sortAsc);
+        
+        int overlap = 0;
+        auto prev = intervals[0]; 
+        for(int i = 1; i<n; i++)
         {
-            if(!bag.empty())
+            //check overlap
+            if(intervals[i][0] < prev[1])
             {
-                auto l = bag.back();
-                //no overlap
-                if(l[1]<=i[0])
-                {
-                    bag.push_back(i);
-                    
-                }
-                //overlap
-                else
-                {
-                    if(l[1]>i[1])
-                    {
-                        bag.pop_back();
-                        bag.push_back(i);
-                    }
-                    noncomp++;
-                }
+                overlap++;
             }
             else
             {
-                bag.push_back(i);
+                prev = intervals[i];
             }
         }
-        return noncomp;
+        
+        return overlap;
     }
     
-    static bool comp(vector<int> a, vector<int> b)
+    static bool sortAsc(vector<int>& a, vector<int>& b)
     {
-        return a[0] < b[0];
+        return a[1] < b[1];
     }
 };
