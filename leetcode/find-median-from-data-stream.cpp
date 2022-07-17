d9f4641b7279f324f6bb91604494276757639967
Suraj Aralihalli <suraj.ara16@gmail.com>
2022-07-17 11:00:53 -0400

    Update find-median-from-data-stream.cpp

--- leetcode/find-median-from-data-stream.cpp
+++ leetcode/find-median-from-data-stream.cpp
@@ -10,7 +10,48 @@ using namespace std;
 
 // Add any helper functions you may need here
 
+//new
+class MedianFinder {
+public:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int n;
+    MedianFinder() {
+        n=0;
+        
+    }
+    
+    void addNum(int num) {
+        n++;
+        maxHeap.push(num);
+        int t = maxHeap.top();
+        maxHeap.pop();
+        minHeap.push(t);
+        
+        int l = n/2;
+        if(minHeap.size()>l)
+        {
+            t = minHeap.top();
+            minHeap.pop();
+            maxHeap.push(t);
+        }
+        
+    }
+    
+    double findMedian() {
+        if(n%2!=0)
+        {
+            return (double) maxHeap.top();
+        }
+        else
+        {
+            return (double)(((double) maxHeap.top() + (double) minHeap.top())/2);
+        }
+    }
+};
 
+
+//old
 vector <int> findMedian(vector <int> arr) {
   // Write your code here
   priority_queue<int> small;
