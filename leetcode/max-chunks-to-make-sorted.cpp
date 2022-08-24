0ed6a1f204a6343017bc9849ed9a3b18bb0aac67
Suraj Aralihalli <suraj.ara16@gmail.com>
2022-08-24 19:44:47 -0400

    Update max-chunks-to-make-sorted.cpp

--- leetcode/max-chunks-to-make-sorted.cpp
+++ leetcode/max-chunks-to-make-sorted.cpp
@@ -3,6 +3,33 @@
 // Date: 24th August, 2022
 // Tags: stack, greedy, monotonic-stack
 
+//Approach 2
+// Author: Suraj Aralihalli
+// Url: https://leetcode.com/problems/max-chunks-to-make-sorted/
+// Date: 24th August, 2022
+// Tags: stack, greedy, monotonic-stack
+
+
+//Approach 2
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int maxi = -1;
+        int count=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            maxi = max(maxi, arr[i]);
+            if(i==maxi)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+
+//Approach 1
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
