4b71ebaec2b06dcd29df01a6a8b2e33f40ca2ee2
Suraj Aralihalli <suraj.ara16@gmail.com>
2022-09-25 15:57:01 -0400

    Create min-stack.cpp

---
+++ leetcode/min-stack.cpp
@@ -0,0 +1,40 @@
+// Author: Suraj Aralihalli
+// Url: https://leetcode.com/problems/min-stack/
+// Date: 25th September, 2022
+// Tags: stack
+
+class MinStack {
+public:
+    stack<int> stk;
+    stack<int> mstk;
+    MinStack() {
+       
+    }
+    
+    void push(int val) {
+        stk.push(val);
+        if(mstk.empty() || mstk.top()>=val) mstk.push(val);
+    }
+    
+    void pop() {
+        if(mstk.top()==stk.top()) mstk.pop();
+        stk.pop();
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+        return mstk.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
