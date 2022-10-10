697bea9cfecfeffdadc1767eef05a39b5bd98dc9
Suraj Aralihalli <suraj.ara16@gmail.com>
2022-10-10 16:55:03 -0400

    Update max-area-of-island.cpp

--- leetcode/max-area-of-island.cpp
+++ leetcode/max-area-of-island.cpp
@@ -1,16 +1,17 @@
 // Author: Suraj Aralihalli
 // Url: https://leetcode.com/problems/max-area-of-island/
-// Date: 1st June, 2021
-// Tags: dfs
+// Date: 10th October, 2022
+// Tags: graph, dfs
 
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> visited;
-        int max_area=0;
-        visited.assign(m,vector<int>(n,0));
+        
+        vector<vector<int>> visited(m,vector<int>(n,0));
+        int max_area = 0;
+        
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -18,7 +19,7 @@ public:
                 if(grid[i][j]==1 && visited[i][j]==0)
                 {
                     int area=0;
-                    help(i,j,grid,visited,area);
+                    dfs(i,j,grid,visited,area);
                     max_area= max(max_area,area);
                     
                 }
@@ -27,32 +28,26 @@ public:
         return max_area;
         
     }
-    static void help(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& visited,int& area)
+    
+    void dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& visited,int& area)
     {
-        if(visited[i][j]) {return;}
-        visited[i][j]=1;
-        if(grid[i][j])
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        if(i>=m || i<0 || j>=n || j<0 || visited[i][j]==1 || grid[i][j]==0) 
         {
-            int m = grid.size();
-            int n = grid[0].size();
-            area++;
-            if(i-1>=0)
-            {
-                help(i-1,j,grid,visited,area);
-            }
-            if(j-1>=0)
-            {
-                help(i,j-1,grid,visited,area);
-            }
-            if(i+1<m)
-            {
-                help(i+1,j,grid,visited,area);
-            }
-            if(j+1<n)
-            {
-                help(i,j+1,grid,visited,area);
-            }
+            return;
         }
-        return;
+        else
+        {
+            visited[i][j]=1;
+            area++;
+            
+            dfs(i-1,j,grid,visited,area);
+            dfs(i+1,j,grid,visited,area);
+            dfs(i,j-1,grid,visited,area);
+            dfs(i,j+1,grid,visited,area);
+            return;
+        }
     }
 };
