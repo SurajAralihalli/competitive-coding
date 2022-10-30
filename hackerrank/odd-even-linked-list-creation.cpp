e41979eccd3c949078aef256dcc4539513b542be
Suraj Aralihalli <suraj.ara16@gmail.com>
2022-10-30 14:43:10 -0400

    Create odd-even-linked-list-creation.cpp

---
+++ hackerrank/odd-even-linked-list-creation.cpp
@@ -0,0 +1,61 @@
+// Author: Suraj Aralihalli
+// Url: https://leetcode.com/problems/odd-even-linked-list/
+// Date: 2nd Sept, 2022
+// Tags: linked-list, pointer
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL) return head;
+        else if(head->next==NULL) return head;
+        
+        ListNode* oddhead = head;
+        ListNode* evenhead = NULL;
+        
+        ListNode* oddcur = head;
+        ListNode* evencur = NULL;
+        
+        ListNode* oddprev = NULL;
+        
+        
+        while(oddcur!=NULL && oddcur->next!=NULL)
+        {
+            if(evenhead==NULL)
+            {
+                evenhead = oddcur->next;
+                evencur = oddcur->next;
+            }
+            else
+            {
+                evencur->next = oddcur->next;
+                evencur = evencur->next;
+            }
+            oddcur->next = oddcur->next->next;
+            oddprev = oddcur;
+            oddcur = oddcur->next;
+        }
+        
+        evencur->next = NULL;
+        
+        if(oddcur==NULL)
+        {
+            oddprev->next = oddEvenList(evenhead);
+        }
+        else
+        {
+            oddcur->next = oddEvenList(evenhead);
+        }
+        
+        return oddhead;
+    }
+};
