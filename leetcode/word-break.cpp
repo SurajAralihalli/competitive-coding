c56d751b50fd261ef89c08f189fbd8b348201ab3
Suraj Aralihalli <suraj.ara16@gmail.com>
2023-01-30 22:00:44 -0500

    Update word-break.cpp

--- leetcode/word-break.cpp
+++ leetcode/word-break.cpp
@@ -3,7 +3,52 @@
 // Date: 21st November, 2021
 // Tags: dp, hash-table
 
+// Approach 2
+class Solution {
+public:
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        bool sol=   false;
+        set<string> hash;
+        int n = s.size();
+        vector<bool> dp(n+1,false);
+        for(string w: wordDict)
+        {
+            hash.insert(w);
+        }
+        
+        for(int j=0; j < n; j++)
+         {
+            for(int l=1; j+l <= n; l++)
+             {
+                 int b = j;
+                 int e = j + l - 1;
+                 // first index or b-1 index
+                 if(b == 0 || dp[b-1]==true)
+                 {
+                    string w = s.substr(j, l);
+                    if(hash.find(w)!=hash.end())
+                    {
+                        cout << w << " " << b << " " << e << endl;
+                        dp[e] = true;
+                    }
+                 }
+             }
+         }
 
+         for(int i=0;i<n;i++)
+         {
+             cout << dp[i] << " ";
+         }
+         cout << endl;
+    
+        return dp[n-1];
+    }
+    
+};
+
+
+// Approach 1
 class Solution {
 public:
     
