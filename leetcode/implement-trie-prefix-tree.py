8dd1413ed3a06cfb25ca0d59149ec9cf6f1b25a5
Suraj Aralihalli <suraj.ara16@gmail.com>
2023-02-07 19:22:36 -0500

    Update implement-trie-prefix-tree.py

--- leetcode/implement-trie-prefix-tree.py
+++ leetcode/implement-trie-prefix-tree.py
@@ -3,6 +3,47 @@
 # Date: 21st November, 2021
 # Tags: trie, prefix-tree
 
+
+# Approach 2
+class TrieNode:
+    children = None
+    isWord = None
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.isValid = False
+
+class Trie:
+    root = None
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            node = node.children[c]
+        node.isWord = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for c in word:
+            if c in node.children:
+                node = node.children[c]
+            else:
+                return False
+        if node.isWord:
+            return True
+        else:
+            return False
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for c in prefix:
+            if c in node.children:
+                node = node.children[c]
+            else:
+                return False
+        return True
+    
+# Approach 1    
 class Node:
     def __init__(self):
         self.children = {}
