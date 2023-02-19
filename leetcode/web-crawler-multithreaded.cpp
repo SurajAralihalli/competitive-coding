c4000512eebfdab5a229dcdd8e55c5983be0d99e
Suraj Aralihalli <suraj.ara16@gmail.com>
2023-02-19 10:51:34 -0500

    Update web-crawler-multithreaded.cpp

--- leetcode/web-crawler-multithreaded.cpp
+++ leetcode/web-crawler-multithreaded.cpp
@@ -11,36 +11,57 @@
  *     vector<string> getUrls(string url);
  * };
  */
-class Solution {
-public:
-    unordered_set<string> crawled;
-    queue<string> tasks;
-    int working_threads;
-    bool done;
-    pthread_mutex_t lock;
-    pthread_cond_t cv;
 
-    void* parallel_crawl(void* hp) {
+unordered_set<string> crawled;
+queue<string> tasks;
+int working_threads;
+bool done;
+pthread_mutex_t mutex_lock;
+pthread_cond_t cv;
+
+bool areSameHostname(string a, string b) {
+    stringstream ss(a);
+    string tokenA = "";
+    for(int i=0;i<3;i++)
+    {
+        getline(ss, tokenA, '/');
+    }
+
+    stringstream ss2(a);
+    string tokenB = "";
+    for(int i=0;i<3;i++)
+    {
+        getline(ss2, tokenB, '/');
+    }
+
+    return tokenA == tokenB;
+}
+
+void* parallel_crawl(void* hp) {
         while(true) {
-            pthread_mutex_lock(&lock);
-            while( (tasks.empty() || !done) ) {
-                pthread_cond_wait(&cv, &lock);
+            pthread_mutex_lock(&mutex_lock);
+            while( tasks.empty() && !done ) {
+                cout << "here" << endl;
+                pthread_cond_wait(&cv, &mutex_lock);
             }
 
             if(done) {
-                pthread_mutex_unlock(&lock);
+                cout << "exit" << endl;
+                pthread_cond_broadcast(&cv);
+                pthread_mutex_unlock(&mutex_lock);
                 return NULL;
             }
 
             working_threads++;
             string host_url = tasks.front(); tasks.pop();
+            cout << host_url << endl;
             pthread_cond_signal(&cv);
-            pthread_mutex_unlock(&lock);
+            pthread_mutex_unlock(&mutex_lock);
 
             HtmlParser* hParser = (HtmlParser*) hp;
             auto urls = hParser->getUrls(host_url);
 
-            pthread_mutex_lock(&lock);
+            pthread_mutex_lock(&mutex_lock);
             for(auto url: urls) {
                 if(areSameHostname(url, host_url) && crawled.count(url)==0) {
                     tasks.push(url);
@@ -51,51 +72,37 @@ public:
             working_threads--;
             if(tasks.empty() && working_threads==0) {
                 done = true;
+                cout << "done" << endl;
             }
             pthread_cond_signal(&cv);
-            pthread_mutex_unlock(&lock);
+            pthread_mutex_unlock(&mutex_lock);
         }
-        
     }
 
+
+
+class Solution {
+public:
     vector<string> crawl(string startUrl, HtmlParser htmlParser) {
-        pthread_mutex_init(&lock, NULL);
+
+        pthread_mutex_init(&mutex_lock, NULL);
         pthread_cond_init(&cv, NULL);
         working_threads = 0;
-
         crawled.insert(startUrl);
         tasks.push(startUrl);
         done = false;
-
+        
         unsigned int n = std::thread::hardware_concurrency();
 
         pthread_t threads[n];
-        for(auto i: threads) {
-            pthread_create(&threads[i], NULL, parallel_crawl, htmlParser);
+        for(auto worker: threads) {
+            pthread_create(&worker, NULL, parallel_crawl, &htmlParser);
         }
 
-        for(auto i: threads) {
-            pthread_join(threads[i], NULL);
+        for(auto worker: threads) {
+            pthread_join(worker, NULL);
         }
 
         return vector<string> (crawled.begin(), crawled.end());
-    }
-
-    bool areSameHostname(string a, string b) {
-        stringstream ss(a);
-        string tokenA = "";
-        for(int i=0;i<3;i++)
-        {
-            getline(ss, tokenA, '/');
-        }
-
-        stringstream ss2(a);
-        string tokenB = "";
-        for(int i=0;i<3;i++)
-        {
-            getline(ss2, tokenB, '/');
-        }
-
-        return tokenA == tokenB;
     }
 };
